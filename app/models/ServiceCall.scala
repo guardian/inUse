1123aa8ec08c62bbc33a1b2b4cba2b23becb8d04
jharewinton <jonathan.hare-winton@guardian.co.uk>
2016-08-09 16:24:42 +0100

    write service calls to DynamoDB

--- app/models/ServiceCall.scala
+++ app/models/ServiceCall.scala
@@ -4,7 +4,7 @@ import com.amazonaws.services.dynamodbv2.document.Item
 import org.cvogt.play.json.Jsonx
 import play.api.libs.json.{Format, JsValue, Json}
 
-case class ServiceCall(service: String, time: Long, data: String) {
+case class ServiceCall(name: String, createdAt: Long, data: String) {
 
   def toItem = Item.fromJSON(Json.toJson(this).toString())
 
