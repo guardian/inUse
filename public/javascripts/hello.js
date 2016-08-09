4c08596f03b00d5be9c25cb39fe8244303ab46ce
Matthew Walls <matthew.walls@guardian.co.uk>
2016-08-09 13:38:44 +0100

    Style landing page

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,27 @@
-if (window.console) {
-  console.log("Started");
-}
+
+$(function(){
+
+  console.log("Herro, Javascript");
+
+  var filterServices = function(service) {
+
+    $(".servicebox").each(function() {
+
+      if($(this).data("service").includes(service) || service === ""){
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+
+    });
+
+    $()
+
+
+  }
+
+  $("#filter-input").keydown(function(){
+    filterServices($("#filter-input").val().trim())
+  })
+
+})
