7739f4db902f62c7d93869506066812d5697ef0d
jharewinton <jonathan.hare-winton@guardian.co.uk>
2016-08-11 10:21:48 +0100

    all powerful delete everything endpoint

--- app/controllers/Management.scala
+++ app/controllers/Management.scala
@@ -1,9 +1,16 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import services.Dynamo
 
 class Management extends Controller {
   def healthCheck = Action {
     Ok("OK")
   }
+
+  def deleteAllHistory = Action {
+    Dynamo.deleteAllServicesAndCalls
+    Ok("Ok")
+  }
+
 }
