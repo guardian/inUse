f5ddd0e119394949f50a6752bbdc5dd7cf0f06e0
jharewinton <jonathan.hare-winton@guardian.co.uk>
2016-08-11 10:21:48 +0100

    all powerful delete everything endpoint

--- app/services/AWS.scala
+++ app/services/AWS.scala
@@ -3,6 +3,9 @@ package services
 import com.amazonaws.regions.{Region, Regions}
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.dynamodbv2.document._
+import models.{Service, ServiceCall}
+
+import scala.collection.JavaConversions._
 
 
 object AWS {
@@ -17,5 +20,23 @@ object Dynamo {
   lazy val dynamoDb = new DynamoDB(AWS.dynamoClient)
   lazy val services = dynamoDb.getTable(Config.servicesTableName)
   lazy val serviceCalls = dynamoDb.getTable(Config.serviceCallsTableName)
+
+  def deleteService(service: String) = {
+    Dynamo.services.deleteItem("service", service)
+  }
+
+  def deleteServiceCall(service: String, createdAt: Long) = {
+    Dynamo.serviceCalls.deleteItem("service", service, "createdAt", createdAt)
+  }
+
+  def deleteAllServicesAndCalls = {
+    Dynamo.serviceCalls.scan()
+      .map(ServiceCall.fromItem)
+      .foreach(e => deleteServiceCall(e.service, e.createdAt))
+
+    Dynamo.services.scan()
+      .map(Service.fromItem)
+      .foreach(e => deleteService(e.service))
+  }
 
 }
