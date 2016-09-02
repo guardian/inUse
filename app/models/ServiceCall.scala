4de9819efa0c425be3c610a8bc22f19dcb244e36
Jacob Winch <jacob.winch@guardian.co.uk>
2016-09-02 13:28:59 +0100

    Add model for JSON data and include in service view

--- app/models/ServiceCall.scala
+++ app/models/ServiceCall.scala
@@ -2,11 +2,31 @@ package models
 
 import com.amazonaws.services.dynamodbv2.document.Item
 import org.cvogt.play.json.Jsonx
-import play.api.libs.json.{Format, JsValue, Json}
+import play.api.libs.json._
+
+case class ServiceData(
+                        category: Option[String] = None,
+                        user: Option[String] = None,
+                        target: Option[String] = None,
+                        datetime: Option[String],
+                        function: Option[String],
+                        pkg: Option[String],
+                        description: Option[String],
+                        asset: Option[String],
+                        ip: Option[String]
+                      )
+
+object ServiceData {
+
+  implicit val serviceDataFormat = Json.format[ServiceData]
+
+}
 
 case class ServiceCall(service: String, createdAt: Long, data: String) {
 
   def toItem = Item.fromJSON(Json.toJson(this).toString())
+
+  def toServiceData(data: String = data): ServiceData = (Json.parse(data).as[ServiceData])
 
 }
 
